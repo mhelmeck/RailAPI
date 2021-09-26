1081b572ad520acc1802c5653d7e59c29bb03990
Maciej Hełmecki <mhelmecki@gmail.com>
2021-09-26 02:18:53 +0200

    Fix dependencies

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module main
+module github.com/mhelmeck/RailAPI
 
 go 1.16
+
+require github.com/mattn/go-sqlite3 v1.14.8 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"log"
 
+	_ "github.com/mattn/go-sqlite3"
+
+	"github.com/mhelmeck/RailAPI/dbutils"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./railapi.db")
+	if err != nil {
+		log.Println("Driver creation failed!")
+	}
+
+	dbutils.Initialize(db)
 }
